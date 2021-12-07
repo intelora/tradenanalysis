f11ef52aef343e16bc223cb3e645e652b6483fde
aswath.m <aswath.m@intelora.co.in>
2021-12-07 18:34:47 +0530

    Fix connection issues

--- db_connect.py
+++ db_connect.py
@@ -1,18 +1,16 @@
 from sqlalchemy import create_engine
 import pandas as pd
-
+import pymysql
 
 def db_connect():
     try:
-        db_connection_str = 'mysql+pymysql://intelaku_nse_aswath:intelaku_nse_aswath@intelora.co.in:3306/intelaku_nse_aswath'
+        db_connection_str = "mysql+pymysql://root:admin@localhost:3306/newschema"
         db_connection = create_engine(db_connection_str)
     
         return db_connection
     except Exception as e:
         print(e,e.args)
         return None
-
-
 def get_data_as_dataframe(ticker):
     try:
         db_call = "select * from {}".format(ticker)
@@ -21,6 +19,3 @@ def get_data_as_dataframe(ticker):
     except Exception as e:
         print(e,e.args)
         return 
-
-
-
